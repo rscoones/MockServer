f48b1d0c80894f5237b0deccc72cf19e1f4d0ba5
Richard Scoones <richardscoones@gmail.com>
2015-08-31 02:38:14 +0100

    Removing old folder

--- dist/assets/js/MockServerUI.js
+++
@@ -1,9 +0,0 @@
-webpackJsonp([2,3],[
-/* 0 */
-/***/ function(module, exports, __webpack_require__) {
-
-	(function webpackMissingModule() { throw new Error("Cannot find module \"./src/MockServerUI/profiles/dev.js\""); }());
-
-
-/***/ }
-]);
