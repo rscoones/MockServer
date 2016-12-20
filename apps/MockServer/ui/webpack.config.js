0f0781e99661c126b1a0efa51fcd6780ad534ff8
Richard Scoones <richardscoones@gmail.com>
2016-12-20 20:17:27 +0000

    Moving bootstrap styling to webpack import

--- apps/MockServer/ui/webpack.config.js
+++ apps/MockServer/ui/webpack.config.js
@@ -16,12 +16,13 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.jsx?$/, exclude: excludes, loader: "babel" },
-      { test: /\.css&/, loader: "style-loader!css-loader!" }
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
     ]
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"[name].js")
-  ],
   resolve: {
     alias: {
       // generics
